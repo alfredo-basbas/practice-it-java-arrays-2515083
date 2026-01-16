b7389617e24dc17fae75add71bb018b87120b756
alfredo-basbas <159981950+alfredo-basbas@users.noreply.github.com>
2026-01-16 07:35:44 +0000

    iterating

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class App {
 
   public static void main(String[] args) {
@@ -5,5 +7,22 @@ public class App {
     System.out.println(lotteryNums[2]);
     lotteryNums[2] = 40;
     System.out.println(lotteryNums[2]);
+
+    for (int i = 0; i < lotteryNums.length; i++) {
+      System.out.println(lotteryNums[i]);
+    }
+    System.out.println();
+
+    for (int i = lotteryNums.length - 1; i >= 0; i--) {
+      System.out.println(lotteryNums[i]);
+    }
+    System.out.println();
+
+    for (double item : lotteryNums) {
+      System.out.println(item);
+    }
+    System.out.println("STREAMS");
+
+    Arrays.stream(lotteryNums).forEach(System.out::println);
   }
 }
